19e769bb02e0fc7255ab03f44a2f02bf5f23c0a8
Mathias Gilson <mathias.gilson@outlook.com>
2020-04-08 13:34:20 +0700

    now control the last playing tab when resume playing + UI improvements

--- inject.js
+++ inject.js
@@ -4,65 +4,82 @@ const PLAYERS = {
             code: '"KeyN"',
             key: '"n"',
             keyCode: 78,
-            shiftKey: true
+            shiftKey: true,
         },
         prev: {
             code: '"KeyP"',
             key: '"p"',
             keyCode: 80,
-            shiftKey: true
+            shiftKey: true,
         },
         playPause: {
             code: '"KeyK"',
             key: '"k"',
-            keyCode: 75
-        }
+            keyCode: 75,
+        },
     },
     "music.youtube.com": {
         next: {
             code: '"KeyJ"',
             key: '"j"',
-            keyCode: 74
+            keyCode: 74,
         },
         prev: {
             code: '"KeyK"',
             key: '"k"',
-            keyCode: 75
+            keyCode: 75,
         },
         playPause: {
             code: '"Spacebar"',
             key: '" "',
-            keyCode: 32
-        }
+            keyCode: 32,
+        },
     },
     "www.deezer.com": {
         buttonBased: true,
         next: `'.svg-icon-group-btn[aria-label="Next"]'`,
         prev: `'.svg-icon-group-btn[aria-label="Back"]'`,
-        playPause: `'.svg-icon-group-btn[aria-label="Pause"], .svg-icon-group-btn[aria-label="Play"]'`
+        playPause: `'.svg-icon-group-btn[aria-label="Pause"], .svg-icon-group-btn[aria-label="Play"]'`,
     },
     "open.spotify.com": {
         buttonBased: true,
         next: '".spoticon-skip-forward-16"',
         prev: '".spoticon-skip-back-16"',
-        playPause: '".spoticon-play-16, .spoticon-pause-16"'
+        playPause: '".spoticon-play-16, .spoticon-pause-16"',
     },
     "listen.tidal.com": {
         buttonBased: true,
         next: `'.playback-controls__button--white-icon[data-test="next"]'`,
         prev: `'.playback-controls__button--white-icon[data-test="previous"]'`,
-        playPause: `'.playback-controls__button--white-icon[data-test="play"], .playback-controls__button--white-icon[data-test="pause"]'`
-    }
+        playPause: `'.playback-controls__button--white-icon[data-test="play"], .playback-controls__button--white-icon[data-test="pause"]'`,
+    },
 }
 
-chrome.commands.onCommand.addListener(command => {
-    chrome.tabs.query({}, tabs => {
+let lastPlayingTab = {}
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log("key pressed")
+    chrome.tabs.query({}, (tabs) => {
         const playerTab = tabs
+            .filter((tab) => !tab.mutedInfo || !tab.mutedInfo.muted)
+            .map((tab) => {
+                console.log(tab)
+                return { url: tab.url, hostname: new URL(tab.url).hostname, id: tab.id, audible: tab.audible }
+            })
+            .reduce((acc, element) => {
+                if (lastPlayingTab && lastPlayingTab.id === element.id && lastPlayingTab.url === element.url) {
+                    return [element, ...acc]
+                }
+                return [...acc, element]
+            }, [])
             .sort((a, b) => b.audible - a.audible)
-            .map(tab => ({ hostname: new URL(tab.url).hostname, id: tab.id }))
-            .find(tab => Object.keys(PLAYERS).includes(tab.hostname))
+            .find((tab) => Object.keys(PLAYERS).includes(tab.hostname))
+
+        console.log("playerTab", playerTab)
 
         if (!playerTab) return
+
+        lastPlayingTab = playerTab
 
         const player = PLAYERS[playerTab.hostname]
         let action
@@ -70,11 +87,10 @@ chrome.commands.onCommand.addListener(command => {
         switch (command) {
             case "NextTrack":
                 action = player.next
-
                 break
+
             case "PreviousTrack":
                 action = player.prev
-
                 break
 
             case "PlayPause":
@@ -94,14 +110,12 @@ chrome.commands.onCommand.addListener(command => {
                 action.code
             },composed: true, isTrusted: true, defaultPrevented: true, charCode: 0, shiftKey : ${!!action.shiftKey}, key: ${
                   action.key
-              }, keyCode: ${
-                  action.keyCode
-              },location: 0, metaKey: false}); console.log(e); document.dispatchEvent(e);';
+              }, keyCode: ${action.keyCode},location: 0, metaKey: false}); console.log(e); document.dispatchEvent(e);';
             
             document.head.appendChild(myScript);`
 
         chrome.tabs.executeScript(playerTab.id, {
-            code: injectedCode
+            code: injectedCode,
         })
     })
 })
--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("#support").addEventListener("click", function() {
-        window.open(
-            "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JEAWYHFCZ8LT4&source=url"
-        )
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#support").addEventListener("click", function () {
+        window.open("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JEAWYHFCZ8LT4&source=url")
     })
 
-    document.querySelector("#about").addEventListener("click", function() {
+    document.querySelector("#rate").addEventListener("click", function () {
+        window.open("https://chrome.google.com/webstore/detail/pohakmokiogdbhiocmacgalcmnfdbbne")
+    })
+
+    document.querySelector("#about").addEventListener("click", function () {
         window.open("https://github.com/MathiasGilson/MusicControllerChromeExtension")
     })
 
-    document.querySelector("#feedback").addEventListener("click", function() {
+    document.querySelector("#feedback").addEventListener("click", function () {
         window.open("https://github.com/MathiasGilson/MusicControllerChromeExtension/issues")
     })
 })
